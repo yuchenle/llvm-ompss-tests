0dff67e8f7846a8fdaaab413367f2502d6b616ae
Adrian Munera <adrian.munera@bsc.es>
2022-12-16 11:46:07 +0000

    Merge all axpy tests into a single binary

---
+++ axpy/axpy.cpp
@@ -0,0 +1,185 @@
+// Part of the CG (Conjugate gradient) method
+// Complete source in BAR/Apps/CG from https://pm.bsc.es/svn/BAR
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "gtest/gtest.h"
+
+#if defined(_OPENMP)
+    #include <omp.h>
+#endif
+
+#ifndef N
+    //#define N 16777216
+    #define N 268435456
+#endif
+#ifndef BS1
+    #define BS1 (N/1024)
+#endif
+#ifndef BS2
+    #define BS2 (BS1/32)
+#endif
+#define a   1.0
+
+#define NUM_ITER 10
+
+struct timeval t1, t2;
+
+void saxpy(double *x, double *y)
+{
+    for (int i = 0; i < N; i+=BS1)
+    {
+#ifdef PREALLOC
+        #pragma omp task dep_check(dynamic)
+#else
+        #pragma omp task //shared(x,y)
+#endif  
+        { 
+            for(int j = 0; j < BS1; j++)
+            {
+                y[i+j] += a * x[i+j];
+            }
+        }
+    }
+    #pragma omp taskwait
+}
+
+TEST(AXPY, DYNAMIC_TDG)
+{
+    double *x, *y;
+    x = (double*)malloc(sizeof(double) * N);
+    y = (double*)malloc(sizeof(double) * N);
+
+    for(int i = 0; i < N; ++i){
+        x[i] = y[i] = 1;
+    }
+
+    #pragma omp parallel shared(x,y)
+    #pragma omp single
+    {
+      for (int i=0; i<NUM_ITER; i++)
+      #ifdef TDG
+      #pragma omp taskgraph tdg_type(dynamic)// num_preallocs(2)
+      #endif
+      {
+        saxpy(x, y);
+      }
+    }
+
+    for (int i=0; i<NUM_ITER; i++)
+        EXPECT_EQ(x[i]*(NUM_ITER)+1,y[i]);
+
+    free(x);
+    free(y);
+}
+
+TEST(AXPY, STATIC_TDG)
+{
+    double *x, *y;
+    x = (double*)malloc(sizeof(double) * N);
+    y = (double*)malloc(sizeof(double) * N);
+
+    for(int i = 0; i < N; ++i){
+        x[i] = y[i] = 1;
+    }
+
+    #pragma omp parallel shared(x,y)
+    #pragma omp single
+    {
+      for (int i=0; i<NUM_ITER; i++)
+      #ifdef TDG
+      #pragma omp taskgraph tdg_type(static)// num_preallocs(2)
+      #endif
+      {
+        saxpy(x, y);
+      }
+    }
+
+    for (int i=0; i<NUM_ITER; i++)
+        EXPECT_EQ(x[i]*(NUM_ITER)+1,y[i]);
+
+    free(x);
+    free(y);
+}
+
+TEST(AXPY, RERECORDING)
+{
+    double *x, *y;
+    x = (double*)malloc(sizeof(double) * N);
+    y = (double*)malloc(sizeof(double) * N);
+
+    for(int i = 0; i < N; ++i){
+        x[i] = y[i] = 1;
+    }
+
+    int tasks_added = 0;
+    #pragma omp parallel shared(x,y, tasks_added)
+    #pragma omp single
+    {
+      for (int i=0; i<NUM_ITER; i++)
+      #ifdef TDG
+      #pragma omp taskgraph tdg_type(dynamic) if(i==5)
+      #endif
+      {
+        if(i>2)
+        {
+            #pragma omp task shared(tasks_added)
+            {
+                tasks_added++;
+            }
+        }
+        saxpy(x, y);
+      }
+    }
+
+    EXPECT_EQ(tasks_added,5);
+
+    for (int i = 0; i < N; ++i)
+        EXPECT_EQ(x[i]*(NUM_ITER)+1, y[i]);
+
+    free(x);
+    free(y);
+}
+
+TEST(AXPY, MULTIPLE_DYNAMIC)
+{
+    double *x, *y;
+    x = (double*)malloc(sizeof(double) * N);
+    y = (double*)malloc(sizeof(double) * N);
+
+    for(int i = 0; i < N; ++i){
+        x[i] = y[i] = 1;
+    }
+
+    #pragma omp parallel shared(x,y)
+    #pragma omp single
+    {
+      for (int i=0; i<NUM_ITER / 2; i++)
+      #ifdef TDG
+      #pragma omp taskgraph tdg_type(dynamic)// num_preallocs(2)
+      #endif
+      {
+        saxpy(x, y);
+      }
+
+      for (int i= NUM_ITER / 2; i<NUM_ITER; i++)
+      #ifdef TDG
+      #pragma omp taskgraph tdg_type(dynamic)// num_preallocs(2)
+      #endif
+      {
+        saxpy(x, y);
+      }
+    }
+
+    for (int i=0; i<NUM_ITER; i++)
+        EXPECT_EQ(x[i]*(NUM_ITER)+1,y[i]);
+
+    free(x);
+    free(y);
+}
+
+int main (int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
--- axpy/old_files/axpy_dynamic.cpp
+++ axpy/old_files/axpy_dynamic.cpp
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include "gtest/gtest.h"
+
 #if defined(_OPENMP)
     #include <omp.h>
 #endif
@@ -24,7 +26,7 @@
 
 struct timeval t1, t2;
 
-inline __attribute__((always_inline)) void saxpy(double *x, double *y)
+void saxpy(double *x, double *y)
 {
     for (int i = 0; i < N; i+=BS1)
     {
@@ -34,28 +36,17 @@ inline __attribute__((always_inline)) void saxpy(double *x, double *y)
         #pragma omp task //shared(x,y)
 #endif  
         { 
-                      //  printf("Es %p y %p and %d \n", x,y,i);
-
             for(int j = 0; j < BS1; j++)
             {
                 y[i+j] += a * x[i+j];
             }
-           // printf("Es %p y %p and %d \n", x,y,i);
         }
     }
     #pragma omp taskwait
 }
 
-int main(int argc, char** argv)
+TEST(AXPY_TESTS, DYNAMIC_TDG)
 {
-    if (argc != 2)
-    {
-        printf("Usage: ./axpy_VERSION out_file\n");
-        exit(-1);
-    }
-    FILE *fp;
-    fp = fopen(argv[1], "a");
-
     double *x, *y;
     x = (double*)malloc(sizeof(double) * N);
     y = (double*)malloc(sizeof(double) * N);
@@ -64,9 +55,31 @@ int main(int argc, char** argv)
         x[i] = y[i] = 1;
     }
 
-    double elapsedTime;
-    // NOTE: Initiate time inside the parallel region, so the preallocation of tasks is not taken into account
-    gettimeofday(&t1, NULL);
+    #pragma omp parallel shared(x,y)
+    #pragma omp single
+    {
+      for (int i=0; i<NUM_ITER; i++)
+      #ifdef TDG
+      #pragma omp taskgraph tdg_type(dynamic)// num_preallocs(2)
+      #endif
+      {
+        saxpy(x, y);
+      }
+    }
+
+    for (int i=0; i<NUM_ITER; i++)
+        EXPECT_EQ(x[i]*(NUM_ITER)+1,y[i]);
+}
+
+TEST(AXPY_TESTS, STATIC_TDG)
+{
+    double *x, *y;
+    x = (double*)malloc(sizeof(double) * N);
+    y = (double*)malloc(sizeof(double) * N);
+
+    for(int i = 0; i < N; ++i){
+        x[i] = y[i] = 1;
+    }
 
     #pragma omp parallel shared(x,y)
     #pragma omp single
@@ -78,26 +91,13 @@ int main(int argc, char** argv)
       {
         saxpy(x, y);
       }
-      //#pragma omp taskwait
     }
 
-    gettimeofday(&t2, NULL);
-    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000.0;      // sec to us // chenle
-    // elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000000.0;   // us to ms
-    elapsedTime += (t2.tv_usec - t1.tv_usec);  				 // chenle
-    
+    for (int i=0; i<NUM_ITER; i++)
+        EXPECT_EQ(x[i]*(NUM_ITER)+1,y[i]);
+}
 
-    for (int i = 0; i < N; ++i){
-        if (y[i] != x[i]*(NUM_ITER)+1)
-        {
-            printf("Erroneous results %f \n", y[i]);
-            //fprintf(fp, "%d::Error!\n", omp_get_max_threads());
-            fclose(fp);
-            exit(1);
-        } 
-    }
-    printf("\033[32;1mAXPY static tdg test: correct \n");
-    fclose(fp);
-
-    return 1;
+int main (int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
